87445a8b179be5549fe48139f4fc73bee98d6edb
mathewperez <matperez755@gmail.com>
2021-10-26 21:34:23 -0700

    Instantiate files and dirs

---
+++ utils/commons.py
@@ -0,0 +1,6 @@
+from apiclient.discovery import build
+from urllib.parse import urlparse, parse_qs
+
+class YoutubeApiCommons:
+    def __init__(self, file_name):
+        self.file_name = self.file_name
